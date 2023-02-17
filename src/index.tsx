7500a3bdee110ce018dd4e37cb68f4c00b37abe4
Hendrik Liebau <mail@hendrik-liebau.de>
2023-02-17 17:24:28 +0100

    Render html in the server unless `x-component` header is present

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import type {ProxyHandler} from 'aws-lambda';
 import * as React from 'react';
-import {renderToReadableStream} from 'react-server-dom-webpack/server.browser';
+import ReactDOMServer from 'react-dom/server.browser';
+import ReactServerDOMServer from 'react-server-dom-webpack/server.browser';
 import {App} from './app.js';
 import {convertStreamToString} from './convert-stream-to-string.js';
 
-export const handler: ProxyHandler = async () => {
-  const readableStream = renderToReadableStream(<App />, null);
+export const handler: ProxyHandler = async (event) => {
+  const readableStream = await (event.headers[`x-component`]
+    ? ReactServerDOMServer.renderToReadableStream(<App />, null)
+    : ReactDOMServer.renderToReadableStream(<App />));
+
   const result = await convertStreamToString(readableStream);
 
   return {statusCode: 200, body: result};
---
+++ src/react-dom.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-dom/server.browser' {
+  export {
+    renderToNodeStream,
+    renderToReadableStream,
+    renderToStaticMarkup,
+    renderToStaticNodeStream,
+    renderToString,
+    version,
+  } from 'react-dom/server';
+}
