5aed9e88171b2f822eb72be4b665788fa5e43630
Hendrik Liebau <mail@hendrik-liebau.de>
2023-02-22 10:57:15 +0100

    Pass the stream directly into the result

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import ReactServerDOMServer from 'react-server-dom-webpack/server.browser';
 import {App} from './app.js';
-import {convertStreamToString} from './convert-stream-to-string.js';
 import {createHtmlStream} from './create-html-stream.js';
 
 export default {
@@ -11,17 +10,12 @@ export default {
       null,
     );
 
-    const result = await convertStreamToString(
-      request.headers.has(`x-component`)
-        ? rscStream
-        : await createHtmlStream(rscStream),
-    );
+    const responseStream = request.headers.has(`x-component`)
+      ? rscStream
+      : await createHtmlStream(rscStream);
 
-    return new Response(result, {
-      headers: {
-        'content-type': `text/html; charset=utf-8`,
-        'transfer-encoding': `chunked`,
-      },
+    return new Response(responseStream, {
+      headers: {'content-type': `text/html; charset=utf-8`},
     });
   },
 };
