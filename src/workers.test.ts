9b656f7e757129eff04fe457f60f6f53c026fdf1
Hendrik Liebau <mail@hendrik-liebau.de>
2023-03-07 12:57:32 +0100

    Use Jest and wrangler's `unstable_dev` to test workers

---
+++ src/workers.test.ts
@@ -0,0 +1,31 @@
+import type {UnstableDevWorker} from 'wrangler';
+import {unstable_dev} from 'wrangler';
+
+describe(`main worker`, () => {
+  let rscWorker: UnstableDevWorker;
+  let mainWorker: UnstableDevWorker;
+
+  beforeAll(async () => {
+    [rscWorker, mainWorker] = await Promise.all([
+      unstable_dev(`dist/rsc-worker.js`, {
+        config: `src/rsc-worker/wrangler.toml`,
+        experimental: {disableExperimentalWarning: true},
+      }),
+      unstable_dev(`dist/main-worker.js`, {
+        config: `src/main-worker/wrangler.toml`,
+        experimental: {disableExperimentalWarning: true},
+      }),
+    ]);
+  });
+
+  afterAll(async () => {
+    await Promise.all([rscWorker.stop(), mainWorker.stop()]);
+  });
+
+  it(`responds with html`, async () => {
+    const resp = await mainWorker.fetch();
+    const text = await resp.text();
+
+    expect(text).toMatch(`<p>This is a suspended server component.</p>`);
+  });
+});
