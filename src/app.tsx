55f4198fd1564cf04d41d407d2925ee5df4cfbe8
Hendrik Liebau <mail@hendrik-liebau.de>
2023-03-07 09:53:18 +0100

    Move first big chunk to `App` so that it is shared accross pages

--- src/app.tsx
+++ src/app.tsx
@@ -15,6 +15,18 @@ export function App({pathname}: AppProps): JSX.Element {
         <title>Server Components with Streaming SSR Demo</title>
       </head>
       <body style={{fontFamily: `sans-serif`}}>
+        <span style={{fontSize: `10px`}}>
+          This is a first big chunk to prevent Safari from buffering the whole
+          response before starting to render. Lorem ipsum dolor sit amet,
+          consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+          massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+          eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede
+          justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim
+          justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
+          felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus
+          elementum semper nisi.
+        </span>
         {pathname === `/another-page` ? <AnotherPage /> : <HomePage />}
       </body>
     </html>
