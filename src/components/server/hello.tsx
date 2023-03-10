b887bafea8ea5b48d62a4da8f26a45539f3b8480
Hendrik Liebau <mail@hendrik-liebau.de>
2023-03-10 19:47:44 +0100

    Remove artificial delay from hello component

--- src/components/server/hello.tsx
+++ src/components/server/hello.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import {Markdown} from './markdown.js';
-import {wait} from './wait.js';
 // import 'server-only'; // https://twitter.com/unstubbable/status/1630897868155305984
 
 // Imagine this being a fetch that can only be executed from the server.
 async function fetchSubject(): Promise<string> {
-  await wait(100);
-
-  return `world`;
+  return Promise.resolve(`world`);
 }
 
 export async function Hello(): Promise<JSX.Element> {
