e29531d36656c579f581af468a79b8791f633a31
Hendrik Liebau <mail@hendrik-liebau.de>
2023-03-21 10:09:00 +0100

    Split up react-server-dom-webpack-client types

--- types/react-server-dom-webpack-client.browser.d.ts
+++ types/react-server-dom-webpack-client.browser.d.ts
@@ -29,17 +29,3 @@ declare module 'react-server-dom-webpack/client.browser' {
     value: ReactServerValue,
   ): Promise<string | FormData>;
 }
-
-declare module 'react-server-dom-webpack/client.edge' {
-  import type {Thenable} from 'react';
-  import type {SSRManifest} from 'react-server-dom-webpack';
-
-  export interface CreateFromReadableStreamOptions {
-    moduleMap?: SSRManifest;
-  }
-
-  export function createFromReadableStream<T>(
-    stream: ReadableStream,
-    options?: CreateFromReadableStreamOptions,
-  ): Thenable<T>;
-}
---
+++ types/react-server-dom-webpack-client.edge.d.ts
@@ -0,0 +1,13 @@
+declare module 'react-server-dom-webpack/client.edge' {
+  import type {Thenable} from 'react';
+  import type {SSRManifest} from 'react-server-dom-webpack';
+
+  export interface CreateFromReadableStreamOptions {
+    moduleMap?: SSRManifest;
+  }
+
+  export function createFromReadableStream<T>(
+    stream: ReadableStream,
+    options?: CreateFromReadableStreamOptions,
+  ): Thenable<T>;
+}
