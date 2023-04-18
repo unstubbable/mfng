867826d8820a18addc245047c716c8443469fce1
Hendrik Liebau <mail@hendrik-liebau.de>
2023-04-18 20:54:55 +0200

    Return tracked click count to client

--- apps/shared-app/src/server/track-click.ts
+++ apps/shared-app/src/server/track-click.ts
@@ -5,6 +5,10 @@ import 'server-only';
 let clickCount = 0;
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export async function trackClick(): Promise<void> {
-  console.log(`Clicked ${++clickCount} times.`);
+export async function trackClick(): Promise<number> {
+  clickCount++;
+
+  console.log(`Clicked ${clickCount} ${clickCount === 1 ? `time` : `times`}.`);
+
+  return clickCount;
 }
