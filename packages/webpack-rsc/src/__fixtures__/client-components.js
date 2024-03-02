b3d233359657efdc9eb6436d37fb7f53d011f38b
Hendrik Liebau <mail@hendrik-liebau.de>
2024-03-02 22:12:05 +0100

    Handle export specifiers for client components

--- packages/webpack-rsc/src/__fixtures__/client-components.js
+++ packages/webpack-rsc/src/__fixtures__/client-components.js
@@ -11,6 +11,22 @@ export const ComponentB = function () {
   return React.createElement(`div`);
 };
 
+export const foo = 1;
+
 export const ComponentC = () => {
   return React.createElement(ClientComponentWithServerAction);
 };
+
+const bar = 2;
+
+const ComponentF = () => React.createElement(`div`);
+
+export {D as ComponentD, bar, ComponentE, ComponentF};
+
+function D() {
+  return React.createElement(`div`);
+}
+
+function ComponentE() {
+  return React.createElement(`div`);
+}
