ebd9490b067ab28a79ea2029aad343517e76c643
Hendrik Liebau <mail@hendrik-liebau.de>
2024-03-12 19:52:45 +0100

    Fix lint issues in `stack.ts`

--- apps/aws-app/cdk/stack.ts
+++ apps/aws-app/cdk/stack.ts
@@ -1,8 +1,8 @@
-import * as cdk from 'aws-cdk-lib';
-import {Construct} from 'constructs';
 import path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import type {Construct} from 'constructs';
 
-const distDirname = path.join(import.meta.dirname, '../dist/');
+const distDirname = path.join(import.meta.dirname, `../dist/`);
 
 export class Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,9 +10,9 @@ export class Stack extends cdk.Stack {
 
     const lambdaFunction = new cdk.aws_lambda_nodejs.NodejsFunction(
       this,
-      'function',
+      `function`,
       {
-        entry: path.join(distDirname, 'handler/index.js'),
+        entry: path.join(distDirname, `handler/index.js`),
         runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
         bundling: {format: cdk.aws_lambda_nodejs.OutputFormat.ESM},
         timeout: cdk.Duration.seconds(28),
@@ -21,7 +21,7 @@ export class Stack extends cdk.Stack {
 
     const lambdaFunctionUrl = new cdk.aws_lambda.FunctionUrl(
       this,
-      'function-url',
+      `function-url`,
       {
         function: lambdaFunction,
         authType: cdk.aws_lambda.FunctionUrlAuthType.NONE,
@@ -29,25 +29,25 @@ export class Stack extends cdk.Stack {
       },
     );
 
-    const bucket = new cdk.aws_s3.Bucket(this, 'assets-bucket', {
-      bucketName: 'mfng-aws-app-assets',
+    const bucket = new cdk.aws_s3.Bucket(this, `assets-bucket`, {
+      bucketName: `mfng-aws-app-assets`,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
-    const distribution = new cdk.aws_cloudfront.Distribution(this, 'cdn', {
+    const distribution = new cdk.aws_cloudfront.Distribution(this, `cdn`, {
       defaultBehavior: {
         origin: new cdk.aws_cloudfront_origins.FunctionUrlOrigin(
           lambdaFunctionUrl,
         ),
         allowedMethods: cdk.aws_cloudfront.AllowedMethods.ALLOW_ALL,
-        cachePolicy: new cdk.aws_cloudfront.CachePolicy(this, 'cache-policy', {
+        cachePolicy: new cdk.aws_cloudfront.CachePolicy(this, `cache-policy`, {
           enableAcceptEncodingGzip: true,
           enableAcceptEncodingBrotli: true,
           queryStringBehavior:
             cdk.aws_cloudfront.CacheQueryStringBehavior.all(),
           headerBehavior:
-            cdk.aws_cloudfront.CacheHeaderBehavior.allowList('accept'),
+            cdk.aws_cloudfront.CacheHeaderBehavior.allowList(`accept`),
         }),
         originRequestPolicy:
           cdk.aws_cloudfront.OriginRequestPolicy.ALL_VIEWER_EXCEPT_HOST_HEADER,
@@ -61,19 +61,19 @@ export class Stack extends cdk.Stack {
       priceClass: cdk.aws_cloudfront.PriceClass.PRICE_CLASS_100,
     });
 
-    new cdk.aws_s3_deployment.BucketDeployment(this, 'assets-deployment', {
+    new cdk.aws_s3_deployment.BucketDeployment(this, `assets-deployment`, {
       destinationBucket: bucket,
-      destinationKeyPrefix: 'client',
+      destinationKeyPrefix: `client`,
       sources: [
         cdk.aws_s3_deployment.Source.asset(
-          path.join(distDirname, 'static/client'),
+          path.join(distDirname, `static/client`),
         ),
       ],
       distribution,
       cacheControl: [cdk.aws_s3_deployment.CacheControl.immutable()],
     });
 
-    new cdk.CfnOutput(this, 'cdn-domain-name', {
+    new cdk.CfnOutput(this, `cdn-domain-name`, {
       value: distribution.domainName,
     });
   }
@@ -81,4 +81,4 @@ export class Stack extends cdk.Stack {
 
 const app = new cdk.App();
 
-new Stack(app, 'mfng-aws-app');
+new Stack(app, `mfng-aws-app`);
