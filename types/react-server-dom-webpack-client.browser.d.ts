ac30438000416ca4990522a22aec1cde2979a03c
Hendrik Liebau <mail@hendrik-liebau.de>
2024-09-26 13:24:28 +0200

    Update React to `0.0.0-experimental-778e1ed2-20240926`

--- types/react-server-dom-webpack-client.browser.d.ts
+++ types/react-server-dom-webpack-client.browser.d.ts
@@ -8,12 +8,37 @@ declare module 'react-server-dom-webpack/client.browser' {
 
   export interface ReactServerDomClientOptions {
     callServer?: CallServerCallback;
+    temporaryReferences?: TemporaryReferenceSet;
+    findSourceMapURL?: FindSourceMapURLCallback;
+    replayConsoleLogs?: boolean;
+    environmentName?: string;
   }
+
+  type TemporaryReferenceSet = Map<string, unknown>;
 
   export type CallServerCallback = (
     id: string,
     args: ReactServerValue,
   ) => Thenable<unknown>;
+
+  export type EncodeFormActionCallback = <A>(
+    id: any,
+    args: Promise<A>,
+  ) => ReactCustomFormAction;
+
+  export type ReactCustomFormAction = {
+    name?: string;
+    action?: string;
+    encType?: string;
+    method?: string;
+    target?: string;
+    data?: null | FormData;
+  };
+
+  export type FindSourceMapURLCallback = (
+    fileName: string,
+    environmentName: string,
+  ) => null | string;
 
   export function createFromFetch<T>(
     promiseForResponse: Promise<Response>,
@@ -32,5 +57,8 @@ declare module 'react-server-dom-webpack/client.browser' {
   export function createServerReference(
     id: string,
     callServer: CallServerCallback,
+    encodeFormAction?: EncodeFormActionCallback,
+    findSourceMapURL?: FindSourceMapURLCallback, // DEV-only
+    functionName?: string,
   ): (...args: unknown[]) => Promise<unknown>;
 }
